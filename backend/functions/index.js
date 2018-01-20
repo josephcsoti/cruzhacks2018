93362d7f8e95d6879409706b7d70e07641b855c8
Joseph Csoti <josephcsoti@gmail.com>
2018-01-20 09:50:21 -0800

    Added realtime database functionality

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,5 +1,21 @@
 const functions = require('firebase-functions');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database. 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const text = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  admin.database().ref('/messages').push({message: text}).then(snapshot => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref);
+  });
+});
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from CruzHacks 2018!");
 });
